34987c2a1833f03ee584054c1f534c733233cb6e
Battula <vrbattula@outlook.com>
2020-07-14 11:34:46 -0400

    refs #88271 Include CE only if they have overlapping BGAs
    Update census_employees_overlapping_benefit_group_assignments_report.rb
    
    Update census_employees_overlapping_benefit_group_assignments_report.rb
    
    REFS 88271 - Adds check for more than one bga
    
    stuff

--- app/data_migrations/census_employees_overlapping_benefit_group_assignments_report.rb
+++ app/data_migrations/census_employees_overlapping_benefit_group_assignments_report.rb
@@ -7,14 +7,16 @@ class CensusEmployeesOverlappingBenefitGroupAssignmentsReport < MongoidMigration
 
   def ce_with_overlapping_benefit_assignments(census_employee)
     overlapping_bgas = []
-    census_employee.benefit_group_assignments.each do |target_bga|
-      census_employee.benefit_group_assignments.each do |bga|
-        next if [target_bga.start_on, target_bga.end_on].any? { |date| date.blank? }
-        # TODO: Need to figure out what to do if no end on date
-        bga_end_on = bga.end_on || bga.start_on + 1.year
-        if (bga&.start_on&.to_datetime..bga_end_on.to_datetime).cover?(target_bga&.start_on&.to_datetime) ||
-          (bga&.start_on&.to_datetime..bga_end_on.to_datetime).cover?(target_bga&.end_on&.to_datetime)
-          overlapping_bgas << bga unless overlapping_bgas.include?(bga)
+    if census_employee.benefit_group_assignments.count >= 2
+      census_employee.benefit_group_assignments.each do |target_bga|
+        census_employee.benefit_group_assignments.each do |bga|
+          next if [target_bga.start_on, target_bga.end_on].any? { |date| date.blank? }
+          # TODO: Need to figure out what to do if no end on date
+          bga_end_on = bga.end_on || bga.start_on + 1.year
+          if (bga&.start_on&.to_datetime..bga_end_on.to_datetime).cover?(target_bga&.start_on&.to_datetime) ||
+            (bga&.start_on&.to_datetime..bga_end_on.to_datetime).cover?(target_bga&.end_on&.to_datetime)
+            overlapping_bgas << bga unless overlapping_bgas.include?(bga)
+          end
         end
       end
     end
@@ -36,17 +38,18 @@ class CensusEmployeesOverlappingBenefitGroupAssignmentsReport < MongoidMigration
       field_names = %w(first_name last_name employer_fein aasm_state bga_1_id bga_1_start bga_1_end bga_2_id bga_2_start bga_2_end bga_3_id bga_3_start bga_3_end bga_4_id bga_4_start bga_4_end bga_5_id bga_5_start bga_5_end)
       csv << field_names
       all_census_employees.no_timeout.each do |census_employee|
-        if census_employee.benefit_group_assignments.present?
-          result = ce_with_overlapping_benefit_assignments(census_employee)
-          csv << [
-            result[:census_employee].first_name,
-            result[:census_employee].last_name,
-            result[:census_employee]&.employee_role&.employer_profile&.fein || result[:census_employee]&.employee_role&.employer_profile&.legal_name,
-            result[:census_employee].aasm_state
-          ] + result[:benefit_group_assignments].map do |bga|
-                [bga.id.to_s, bga.start_on.to_s, bga.end_on.to_s ]
-              end.flatten
-        end
+        result = ce_with_overlapping_benefit_assignments(census_employee)
+        # Need to skip unless there are multiple
+        next unless result[:benefit_group_assignments].length >= 2
+
+        csv << [
+          result[:census_employee].first_name,
+          result[:census_employee].last_name,
+          result[:census_employee]&.employee_role&.employer_profile&.fein || result[:census_employee]&.employee_role&.employer_profile&.legal_name,
+          result[:census_employee].aasm_state
+        ] + result[:benefit_group_assignments].map do |bga|
+              [bga.id.to_s, bga.start_on.to_s, bga.end_on.to_s ]
+            end.flatten
       end
     end
   end
