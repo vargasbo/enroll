0e783ffe5f6fec6a385e0804f58e097e12df1cf5
Chaz Haskins <chaz.haskins@gmail.com>
2020-07-16 07:16:21 -0400

    updates available task

--- app/controllers/api/hbxinternal/v1/rake_trigger_controller.rb
+++ app/controllers/api/hbxinternal/v1/rake_trigger_controller.rb
@@ -22,7 +22,7 @@ class Api::Hbxinternal::V1::RakeTriggerController < ActionController::Base
   end
 
   def perform_task
-    available_task = %w[change_person_dob]
+    available_task = %w[change_person_dob remove_person_ssn]
     response = {}
 
     if available_task.include? params[:data][:task]
