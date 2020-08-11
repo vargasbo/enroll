b17c997d525ff297864a0f40a9fa8205acbe6884
RyanEddyIC <ryan.eddy@ideacrew.com>
2020-08-11 17:01:43 -0400

    rubucop errors

--- features/step_definitions/financial_assistance/review_applications.rb
+++ features/step_definitions/financial_assistance/review_applications.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 Given(/^that a family has a Financial Assistance application in the (.*?) state$/) do |state|
   # draft, submitted, determination_response_error, determined
-  hbx_profile = FactoryBot.create(:hbx_profile)
   @family = user.primary_family
   @applications = [FactoryBot.create(:financial_assistance_application, family: @family, aasm_state: state)]
 end
@@ -9,7 +10,7 @@ And(/^the user navigates to the “Help Paying For Coverage” portal$/) do
   visit financial_assistance.applications_path
 end
 
-When(/^the user clicks the “Action” dropdown corresponding to the (.*?) application$/) do |state|
+When(/^the user clicks the “Action” dropdown corresponding to the (.*?) application$/) do
   # draft, submitted, determination_response_error, determined
   find(".dropdown-toggle", :text => "Actions").click
 end
