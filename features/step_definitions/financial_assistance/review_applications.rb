01f5b5ac4948e4a11afc472ad1f3aec3e96e27ef
RyanEddyIC <ryan.eddy@ideacrew.com>
2020-08-11 17:01:43 -0400

    rubucop errors

--- features/step_definitions/financial_assistance/review_applications.rb
+++ features/step_definitions/financial_assistance/review_applications.rb
@@ -4,6 +4,7 @@ Given(/^that a family has a Financial Assistance application in the (.*?) state$
   hbx_profile = FactoryBot.create(:hbx_profile)
   @family = user.primary_family
   @applications = [FactoryBot.create(:financial_assistance_application, family: @family, aasm_state: state)]
+  application.update_attributes(aasm_state: state)
 end
 
 And(/^the user navigates to the “Help Paying For Coverage” portal$/) do
