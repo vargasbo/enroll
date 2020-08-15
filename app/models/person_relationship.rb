c0c8f71b841b1be9ad1c336e020e1e6c891b890d
Jayanth <jayanth.rordev@gmail.com>
2020-08-15 17:02:53 -0400

    refs#faa_podigy added family relationships controller specs

--- app/models/person_relationship.rb
+++ app/models/person_relationship.rb
@@ -101,6 +101,8 @@ class PersonRelationship
   field :successor_id, type: BSON::ObjectId
   field :family_id, type: BSON::ObjectId
 
+  validates_presence_of :predecessor_id, :successor_id, :family_id
+
   track_history :on => [:fields],
                 :scope => :person,
                 :modifier_field => :modifier,
@@ -110,7 +112,7 @@ class PersonRelationship
                 :track_update  => true,    # track document updates, default is true
                 :track_destroy => true
 
-  validates_presence_of :relative_id, message: "Choose a relative"
+  # validates_presence_of :relative_id, message: "Choose a relative"
   validates :kind,
             presence: true,
             allow_blank: false,
