1f3c380dcb742be0fad366aa2ec25f983872df54
Battula <vrbattula@outlook.com>
2020-08-18 15:07:40 -0400

    refs #FAA Dry up css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[notifier/ckeditor/* financial_assistance/*]
+Rails.application.config.assets.precompile += %w[notifier/ckeditor/* financial_assistance/* insured.css glossary.js]
 
 
