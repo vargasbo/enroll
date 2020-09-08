60db22f36b57fe6f640b652626f1bad104fce97a
Vishal Kalletla <vishuk1201@gmail.com>
2020-09-08 10:13:59 -0400

    FAA-ref: robocop fixes

--- app/models/group_selection_prevarication_adapter.rb
+++ app/models/group_selection_prevarication_adapter.rb
@@ -78,10 +78,9 @@ class GroupSelectionPrevaricationAdapter
   def if_family_has_active_shop_sep
     return unless @previous_hbx_enrollment.present? && @previous_hbx_enrollment.is_shop? && @family.latest_shop_sep.present?
     benefit_package = @previous_hbx_enrollment.sponsored_benefit_package
-    if benefit_package.effective_period.cover?(@family.latest_shop_sep.effective_on)
-      @change_plan = 'change_by_qle'
-      yield
-    end
+    return unless benefit_package.effective_period.cover?(@family.latest_shop_sep.effective_on)
+    @change_plan = 'change_by_qle'
+    yield
   end
 
   def if_family_has_active_sep
