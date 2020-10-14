22872ae819e4897c8a972876bd53c84d7fb4cc1e
Bill Transue <transue@gmail.com>
2020-10-14 10:55:23 -0400

    Update New Form and JS

--- app/javascript/controllers/bulk_notice_controller.js
+++ app/javascript/controllers/bulk_notice_controller.js
@@ -1,30 +1,64 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = ['recipientList', 'recipientBadge']
 
   initialize() {
-    console.log('initialize')
     this.orgIds = JSON.parse(this.data.get('orgIds'))
-    console.log(this.orgIds)
   }
 
-  new_identifier(event) {
-    
+  newIdentifier(event) {
     let identifierEl = event.currentTarget
-    console.log(identifierEl.value)
-    if (identifierEl.value.match(/[ ,]/)) { // more than one
+    if (identifierEl.value == "") return
 
-    } else {
-      let match = this.orgIds.find(org => {
-        org.fein == identifierEl.value ||
-        org.hbx_id == identifierEl.value
+    if (identifierEl.value.match(/[ ,]/)) { // more than one
+      let results = { found: [], not_found: [], invalid: [] }
+      identifierEl.value.split(/\s*,?\s/).forEach((id) => {
+        let match = this.findMatchingOrg(id)
+        if (match) {
+          this.newSuccessBadge(match)
+        } else {
+          this.newErrorBadge(id)
+        }
       })
+    } else {
+      let match = this.findMatchingOrg(identifierEl.value)
       if (match) {
-        let newBadge = this.recipientBadgeTarget.cloneNode(true)
-        this.recipientListTarget.appendChild(newBadge)
-        newBadge.classList.remove('d-none')
+        this.newSuccessBadge(match)
+      } else {
+        this.newErrorBadge(identifierEl.value)
       }
     }
+    identifierEl.value = ''
+  }
+
+  newSuccessBadge(match) {
+    let newBadge = this.recipientBadgeTarget.cloneNode(true)
+    newBadge.prepend(match.legal_name)
+    newBadge.querySelector('input').setAttribute('value', match.id)
+    this.recipientListTarget.appendChild(newBadge)
+    newBadge.classList.remove('d-none')
+  }
+
+  newErrorBadge(invalidId) {
+    let newBadge = this.recipientBadgeTarget.cloneNode(true)
+    newBadge.prepend(invalidId)
+    newBadge.classList.remove('btn-info')
+    newBadge.classList.add('btn-danger')
+    this.recipientListTarget.appendChild(newBadge)
+    newBadge.querySelector('input:not([value])').remove()
+    newBadge.classList.remove('d-none')
+  }
+
+  findMatchingOrg(identifier) {
+    return this.orgIds.find(org => {
+      return org.fein == identifier ||
+      org.hbx_id == identifier
+    })
+  }
+
+  deleteIdentifier(event) {
+    let identifierEl = event.currentTarget.closest('button')
+    identifierEl.remove()
   }
 }
