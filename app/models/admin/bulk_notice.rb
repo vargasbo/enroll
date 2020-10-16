9629f839a469020926728a9272acdb8af6f85176
Bill Transue <transue@gmail.com>
2020-10-16 10:36:46 -0400

    Create index & preview views, update controller

--- app/models/admin/bulk_notice.rb
+++ app/models/admin/bulk_notice.rb
@@ -10,6 +10,9 @@ module Admin
     field :body, type: String
     field :aasm_state, type: String, default: 'draft'
     field :document_metadata, type: Hash
+    field :sent_at, type: DateTime
+
+    belongs_to :user, class_name: 'User'
 
     embeds_many :results, class_name: "Admin::BulkNoticeResult"
   end
