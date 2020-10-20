8ab9f7a4b48acdc2e3c9936f2b8aae0a86a3a0c1
Tony Schaffert <tony.schaffert@gmail>
2020-10-20 10:39:55 -0400

    Rubocop compliance

--- app/workers/bulk_notice_worker.rb
+++ app/workers/bulk_notice_worker.rb
@@ -1,10 +1,12 @@
-class BulkNoticeWorker
-    include Sidekiq::Worker
+# frozen_string_literal: true
 
-    def perform(hbx_id)
-        i = rand(1..20)
-        raise StandardError if i > 15
-        sleep(rand(1..30))
-        Rails.logger.info("Processed #{hbx_id}")
-    end
+class BulkNoticeWorker
+  include Sidekiq::Worker
+
+  def perform(hbx_id)
+    i = rand(1..20)
+    raise StandardError if i > 15
+    sleep(rand(1..30))
+    Rails.logger.info("Processed #{hbx_id}")
+  end
 end
