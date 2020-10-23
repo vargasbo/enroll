c12546768a275a5b97de9e037633fd0cf9f11515
Bill Transue <transue@gmail.com>
2020-10-23 10:05:10 -0400

    Fix rubocop errors on BulkNoticeWorker

--- app/workers/bulk_notice_worker.rb
+++ app/workers/bulk_notice_worker.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class BulkNoticeWorker
   include Sidekiq::Worker
   def perform(audience_id, bulk_notice_id)
@@ -15,11 +16,13 @@ class BulkNoticeWorker
     if result.success?
       bulk_notice.results.create(
         audience_id: audience_id,
-        result: "Success")
+        result: "Success"
+      )
     else
       bulk_notice.results.create(
         audience_id: audience_id,
-        result: "Error")
+        result: "Error"
+      )
     end
     Rails.logger.info("Processing #{id} for Bulk Notice request #{bulk_notice_id}")
   end
